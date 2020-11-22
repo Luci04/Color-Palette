a3d02707c6dfd9483672eb572e0bad4fdb3bc385
luci04 <avinashukla2000@gmail.com>
2020-11-23 01:33:06 +0530

    Updates

---
+++ precache-manifest.d3820049727a361c7671c7e636c6df25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f7c507da30a65a0fe626e6dca6d634",
+    "url": "/Color-Palette/index.html"
+  },
+  {
+    "revision": "bbb1db6240ed1f6c96a2",
+    "url": "/Color-Palette/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "d3883d14ed97cb31b3a6",
+    "url": "/Color-Palette/static/css/main.b1b2ba2e.chunk.css"
+  },
+  {
+    "revision": "bbb1db6240ed1f6c96a2",
+    "url": "/Color-Palette/static/js/2.3281aaa9.chunk.js"
+  },
+  {
+    "revision": "2f5d88cd7a9fe173e498156fa6f65024",
+    "url": "/Color-Palette/static/js/2.3281aaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3883d14ed97cb31b3a6",
+    "url": "/Color-Palette/static/js/main.cfe7079b.chunk.js"
+  },
+  {
+    "revision": "0c621340c157e30ae312",
+    "url": "/Color-Palette/static/js/runtime-main.72cf9a60.js"
+  },
+  {
+    "revision": "feef00011d67c672302dc6fdb4469659",
+    "url": "/Color-Palette/static/media/bg.feef0001.svg"
+  }
+]);
